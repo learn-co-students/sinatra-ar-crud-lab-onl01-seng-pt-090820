ea996f458702f47f8253a33e65d100045028c3c9
Pbsmith82 <pbsmith82@gmail.com>
2020-11-24 18:42:02 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,50 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+    erb :index
+  end
+
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+
+  get '/articles/new' do
+    erb :new
+  end
+
+  get '/articles/:id' do
+    @article = Article.find(params[:id])
+    erb :show
+  end
+
+
+  post '/articles' do
+    @article = Article.new(params)
+    @article.save
+
+    redirect "/articles/#{@article.id}"
+  end
+
+  get '/articles/:id/edit' do
+    @article = Article.find(params[:id])
+    erb :edit
+  end
+
+  patch '/articles/:id' do
+    @article = Article.find(params[:id])
+    @article.title = (params[:title])
+    @article.content = (params[:content])
+    @article.save
+
+    redirect "/articles/#{@article.id}"
+  end
+
+  delete '/articles/:id' do
+    @article = Article.find(params[:id])
+    Article.delete(@article)
+    
+
+    redirect "/articles"
   end
 end

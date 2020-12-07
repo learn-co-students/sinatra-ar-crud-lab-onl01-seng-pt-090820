896c1222d5a7b3625ccc7c61aa7c016b48f0478f
DavidRMorphew <morphewd@umich.edu>
2020-12-07 17:07:00 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,34 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+    "This should be readable if shotgun worked."
+  end
+
+  get '/articles' do
+    @articles = Article.all
+
+    erb :"articles/index"
+  end
+
+  get '/articles/new' do
+    erb :"articles/new"
+  end
+
+  get '/articles/:id' do
+    @article = Article.find_by(id: params[:id])
+    
+    erb :"articles/show"
+  end
+  
+  post '/articles' do
+    @article = Article.create(params)
+    
+    redirect "/articles/#{@article.id}"
+  end
+
+  get '/articles/:id/edit' do
+    @article = Article.find_by(id: params[:id])
+    binding.pry
+    erb :"articles/edit"
   end
 end

da7e9dc8bf084ad52bdd31afe1a49b829f722f3f
drewsview34 <drewsview34@outlook.com>
2020-12-24 12:14:24 -0800

    finished lab

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,50 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+
   end
+
+  get '/articles' do
+   @articles = Article.all
+   erb :index
+  end
+
+  get '/articles/new' do
+   erb :new
+  end
+
+  post '/articles' do
+   article_details = params
+   @article = Article.create(article_details)
+
+   redirect to "/articles/#{@article.id}"
+  end
+
+  get '/articles/:id' do
+   id = params[:id]
+   @article = Article.find_by(id: id)
+
+   erb :show
+  end
+
+  get '/articles/:id/edit' do
+   id = params[:id]
+   @article = Article.find_by(id: id)
+   erb :edit
+  end
+
+  patch '/articles/:id' do
+   id = params[:id]
+   @article = Article.find_by(id: id)
+   article_details = params[:article]
+   @article.update(article_details)
+   redirect to "/articles/#{@article.id}"
+  end
+
+   delete '/articles/:id' do
+     id = params[:id]
+     Article.destroy(id)
+     redirect to '/articles'
+   end
+
 end
